2780d3a2eb4ed0ca0f8315295671965b325d70f4
valen2002 <dvalen@yahoo.com.ar>
2022-03-04 17:31:06 -0300

    First commit

---
+++ foreground.js
@@ -0,0 +1,28 @@
+const SHIFT_KEY_CODE = '16'
+const CTRL_KEY_CODE = '17'
+const CHARACTER_KEY_CODE = '89'
+
+let selectedKeys = {};
+onkeydown = event => {
+    if (event.keyCode == CTRL_KEY_CODE || event.keyCode == SHIFT_KEY_CODE || event.keyCode == CHARACTER_KEY_CODE) {
+        selectedKeys[event.keyCode] = true
+    }
+    if (document.activeElement.value) {
+        if (event.keyCode == CHARACTER_KEY_CODE) {
+            if (selectedKeys[CTRL_KEY_CODE]) {
+                let value = document.activeElement.value
+                if (selectedKeys[SHIFT_KEY_CODE]) {
+                    // ctrl + shift + b
+                    value += 'M'
+                } else {
+                    // ctrl + b
+                    value += 'm' 
+                }
+                document.activeElement.value = value
+            }
+        }
+    }
+}
+onkeyup = event => {
+    selectedKeys[event.keyCode] = false
+}
